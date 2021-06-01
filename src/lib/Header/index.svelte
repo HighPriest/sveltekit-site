9863c9e86b6540e9dd6cc763985539ad8a82cfa2
HighPriest <priv.post.acc@gmail.com>
2021-06-02 00:21:23 +0200

    Ghost

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -16,7 +16,7 @@
 		</svg>
 		<ul>
 			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
+			<li class:active={$page.path === '/projects'}><a sveltekit:prefetch href="/projects">Projects</a></li>
 			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,28 @@
+/// <reference path="@types/tryghost__content-api" />
+
+<script lang="ts">
+    import GhostContentAPI from '@tryghost/content-api';
+    //import GhostContentAPI from '@types/tryghost__content-api';
+    
+    
+    const api = new GhostContentAPI({
+        url: 'http://localhost:2368',
+        key: '862c158e70ce6fb7c02323a223',
+        version: "v3"
+    });
+
+    api.posts.browse({limit: 5, include: ['tags','authors']})
+        .then((posts) => {
+            console.log(posts);
+            //return {props: {posts: posts}}
+        }).catch((err) => {
+            console.log(err);
+        });
+</script>
+
+<main>
+    Hello world
+    <!--{#each posts as post}
+        <li><a href="/posts/{post.slug}">{post.title}</a></li>
+    {/each}-->
+</main>
